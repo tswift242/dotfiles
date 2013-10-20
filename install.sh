5d0c14b2a593b23951f307cc82ce3863732b4aaf
Thomas Swift <tswift@u.rochester.edu>
2013-10-19 21:05:03 -0400

    install.sh: improved support for dirs, now sources bashrc and vimrc

--- install.sh
+++ install.sh
@@ -10,19 +10,46 @@ olddir=~/dotfiles_old # backup directory for old dotfiles
 files=".bash .bash_profile .bashrc .gitconfig .gitignore_global .screenrc .vim .vimrc" 
 
 
-# create dotfiles_old in homedir
-echo -n "Creating $olddir to backup any existing dotfiles in ~ ... "
-mkdir -p $olddir
-echo "done"
+# create $olddir
+if [ ! -d $olddir ]; then
+	echo -n "Creating $olddir to backup any existing dotfiles in ~ ... "
+	mkdir $olddir
+	echo "done"
+	echo
+fi
 
-# move any existing dotfiles in homedir to $olddir, then create symlinks from the homedir to any files in the $dir directory specified in $files
+# move any existing dotfiles in ~ to $olddir, 
+# then create symlinks from ~ to any files in $dir specified in $files
 for file in $files; do
-    if [ -e $dir/$file ]; then # only backup dotfiles in ~ if they exist in $dir
-		if [ -e ~/$file ]; then
+	newfile=$dir/$file
+	currfile=~/$file
+	oldfile=$olddir/$file
+    if [ -e $newfile ]; then # only backup old dotfiles in ~ if new ones exist in $dir
+		if [ -e $currfile ]; then
+			if [ -d $oldfile ]; then # remove old file if it's a dir to avoid mv error
+				echo "Removing directory $oldfile"
+				rm -rf $oldfile
+			fi
 			echo "Moving $file from ~ to $olddir"
-			mv ~/$file $olddir/
+			mv $currfile $olddir/
 		fi
-		echo "Creating symlink to $dir/$file in ~"
-		ln -s $dir/$file ~/$file
+		echo "Creating symlink from ~ to $newfile"
+		ln -s $newfile $currfile
 	fi
 done
+echo
+
+
+# source new bash and vim config files if they exist
+bashrc=~/.bashrc
+if [ -f $bashrc ]; then
+	echo -n "Sourcing $bashrc ... "
+	source $bashrc
+	echo "done"
+fi
+vimrc=~/.vimrc
+if [ -f $vimrc ]; then
+	echo -n "Sourcing $vimrc ... "
+	source $vimrc
+	echo "done"
+fi
